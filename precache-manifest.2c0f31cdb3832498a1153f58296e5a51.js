ebc13f4fd8aa3a363c027375d7231fbf5a228429
Sourabh Saini <guru.s.k186@gmail.com>
2020-07-30 02:30:04 +0530

    Updates

---
+++ precache-manifest.2c0f31cdb3832498a1153f58296e5a51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0af75681fbf5317bc2109c20374c226c",
+    "url": "/newtodolist/index.html"
+  },
+  {
+    "revision": "a688cc893b999702d3db",
+    "url": "/newtodolist/static/css/main.dffca86c.chunk.css"
+  },
+  {
+    "revision": "daef5d538bb07da0ef44",
+    "url": "/newtodolist/static/js/2.e60e20bb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/newtodolist/static/js/2.e60e20bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a688cc893b999702d3db",
+    "url": "/newtodolist/static/js/main.cb703dc7.chunk.js"
+  },
+  {
+    "revision": "f076a0959de8f7c37b53",
+    "url": "/newtodolist/static/js/runtime-main.6da63733.js"
+  }
+]);
